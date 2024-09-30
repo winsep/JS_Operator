c7c4fe3c263ca42541d6e9696bfb8be9d0a1b088
TranQuocThang <98342476+winsep@users.noreply.github.com>
2024-09-30 14:01:05 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var a = 9
+var b = 10
+var linebreak = "<br />";
+document.write("a = ", a ,", b = ", b)
+document.write(linebreak)
+tong = a + b
+document.write("tong = " + tong)
+document.write(linebreak)
+hieu = a - b 
+document.write("hieu = " + hieu)
+document.write(linebreak)
+tich = a * b 
+document.write("tich = " + tich)
+document.write(linebreak)
+thuong = a / b
+document.write("thuong = " + thuong)
+document.write(linebreak)
+
+document.write("(a == b) => ");
+result = (a == b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a < b) => ");
+result = (a < b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a > b) => ");
+result = (a > b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a != b) => ");
+result = (a != b);
+document.write(result);
+ocument.write(linebreak);
+         
+document.write("(a >= b) => ");
+result = (a >= b);
+document.write(result);
+document.write(linebreak);
+         
+document.write("(a <= b) => ");
+result = (a <= b);
+document.write(result);
+document.write(linebreak);
